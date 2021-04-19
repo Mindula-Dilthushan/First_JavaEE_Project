defc4c3cb59f0007c5b3bfb8893be5bf794e2637
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-04-19 16:34:56 +0530

    first commit

---
+++ 01_Hello_Servlet/src/lk/HelloServlet.java
@@ -0,0 +1,16 @@
+package lk;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+            response.getWriter().write("Hello Servlet");
+        }
+
+}
