d1712a379b132b0392f626740174bd3b0df4b29c
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-04-19 16:44:24 +0530

    Customer class added

---
+++ 01_Hello_Servlet/src/lk/Customer.java
@@ -0,0 +1,17 @@
+package lk;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/customer")
+public class Customer extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        response.getWriter().write("Hello Customer");
+    }
+
+}
