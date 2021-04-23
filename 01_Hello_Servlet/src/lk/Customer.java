9840baba299298a0d918bdf064cdd776b5ef4d17
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-04-23 14:26:18 +0530

    doGet / doPost

--- 01_Hello_Servlet/src/lk/Customer.java
+++ 01_Hello_Servlet/src/lk/Customer.java
@@ -1,5 +1,6 @@
 package lk;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,7 +12,11 @@ public class Customer extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        response.getWriter().write("Hello Customer");
+        response.getWriter().write("Customer doGet Called");
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Customer doPost Called");
+    }
 }
--- 01_Hello_Servlet/src/lk/HelloServlet.java
+++ 01_Hello_Servlet/src/lk/HelloServlet.java
@@ -1,5 +1,6 @@
 package lk;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,9 +9,15 @@ import java.io.IOException;
 
 @WebServlet(urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
-        @Override
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
-            response.getWriter().write("Hello Servlet");
-        }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        response.getWriter().write("Hello doGet Called");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello doPost Called");
+    }
 
 }
